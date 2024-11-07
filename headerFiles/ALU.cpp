caf0bfa589963a4b964eb8aeb8f70ad72d507fad
Karim M.Abdelaziz <zezokarim002@gmail.com>
2024-11-07 14:32:13 +0200

    ..

--- headerFiles/ALU.cpp
+++
@@ -1,5 +0,0 @@
-#include "ALU.h"
-
-float ALU::addFloat(float a, float b) {
-    return a + b;
-}
--- headerFiles/ALU.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALU_H
-#define ALU_H
-
-class ALU {
-public:
-    float addFloat(float a, float b);
-};
-
-#endif // ALU_H
